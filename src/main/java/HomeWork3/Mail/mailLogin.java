53e3ef4af5859e73f5447fba20f526963a0a0a0a
entreri <entreri666@gmail.com>
2021-11-24 08:55:08 +0300

    123

--- src/main/java/HomeWork3/Mail/mailLogin.java
+++ src/main/java/HomeWork3/Mail/mailLogin.java
@@ -1,6 +1,7 @@
 package HomeWork3.Mail;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
